f702b833f5189bf86b1a7d448015a76806ac0e05
migpar <miguelparrapen@gmail.com>
2021-01-26 00:36:10 +0100

    seguimos arreglando

--- app/src/main/java/com/lunijami/nodehood/SplashScreenActivity.java
+++ app/src/main/java/com/lunijami/nodehood/SplashScreenActivity.java
@@ -1,6 +1,14 @@
 package com.lunijami.nodehood;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class SplashScreenActivity extends Activity {
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
 }
