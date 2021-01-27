b638348c61cfcf900338fb87ce142e8a9c796f40
migpar <miguelparrapen@gmail.com>
2021-01-27 23:45:32 +0100

    Añadimos flujo de ventanas en RegisterActivity

--- app/src/main/java/com/lunijami/nodehood/RegisterActivity.java
+++ app/src/main/java/com/lunijami/nodehood/RegisterActivity.java
@@ -2,13 +2,32 @@ package com.lunijami.nodehood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    protected Button mCancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mCancelButton = (Button) findViewById(R.id.buttonCancel);
+
+        mCancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity
+                        .this, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+
     }
 }
