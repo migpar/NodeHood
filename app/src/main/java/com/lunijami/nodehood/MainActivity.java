15d202b927d8d6b8fd570320835d533625cd032c
migpar <miguelparrapen@gmail.com>
2021-02-10 19:51:42 +0100

    mergeamos rama MainBrach en SplashScreen

--- app/src/main/java/com/lunijami/nodehood/MainActivity.java
+++ app/src/main/java/com/lunijami/nodehood/MainActivity.java
@@ -1,14 +1,73 @@
 package com.lunijami.nodehood;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.bottomappbar.BottomAppBar;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        this.getSupportActionBar().setDisplayShowCustomEnabled(true);
+//
+//        LayoutInflater inflater = LayoutInflater.from(this);
+//        View v = inflater.inflate(R.layout.custom_action_bar, null);
+//
+//        TextView titleTextView = (TextView) v.findViewById(R.id.custom_action_bar_title);
+//        titleTextView.setText(this.getTitle());
+//        titleTextView.setTypeface(App.getInstance().getActionBarTypeFace());
+//
+//        this.getSupportActionBar().setCustomView(v);
+
+        // cast al xml
+        BottomAppBar bottomAppBar = findViewById(R.id.bottom_app_bar_menu);
+
+        //click event en el  FAB
+        findViewById(R.id.chat).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "CHAT Clicked.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //click event en el Hamburguer menu
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Menu clicked!", Toast.LENGTH_SHORT).show();
+//                sheetBehavior = BottomSheetBehavior.from(sheet);
+            }
+
+
+        });
+
+        //click event en el Bottom bar menu item
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.bottom_app_bar_menu_profile:
+                        Toast.makeText(MainActivity.this, "Like clicked.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.bottom_app_bar_menu_search:
+                        Toast.makeText(MainActivity.this, "Search clicked.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.bottom_app_bar_menu_share:
+                        Toast.makeText(MainActivity.this, "Share clicked.", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
+
+
     }
 }
